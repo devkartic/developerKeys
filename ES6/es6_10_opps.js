66742710dc923a969951ddb0031e9bdc7986f912
Kartic Gharami <devkartic@devkartic.local>
2021-07-11 22:33:42 +0600

    Es6 opps, class, property, constructor, prototype and static method

---
+++ ES6/es6_10_opps.js
@@ -0,0 +1,30 @@
+// Opps in ES6
+
+/*
+Coding methodology / style / pattern
+Code more modular and reusable
+Well organized code
+Easier to debug
+*/
+
+class Test{
+
+    constructor(name = 'Sir') {
+        this.name = name;
+        console.log('Hello Everyone. Construct has called when class created.');
+    }
+
+    message(){
+        console.log(`Hi ${this.name}, This is test message.`);
+    }
+
+    static staticMethod(){
+        console.log('Static method called without creating object.');
+    }
+}
+
+let obj = new Test('Kartic');
+obj.message();
+Test.staticMethod();
+
+
