8af7c14c828a778772ac9464c8c3236b1df96367
devkartic <kartic.gharami@gmail>
2024-01-13 00:34:21 +0600

    Working with Exception handling with custom and globally with \Exception extending

--- PHP/cores/index.php
+++ PHP/cores/index.php
@@ -3,3 +3,34 @@
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use App\E15ExceptionAndTryCatchFinallyBlocks\Library\Invoice;
+use App\E15ExceptionAndTryCatchFinallyBlocks\Library\Customer;
+
+$invoice = new Invoice(new Customer(['email'=> 'test@gmail.com']));
+
+// Custom exception handling
+
+try {
+    $invoice->process(-25);
+} catch (\Exception $exception) {
+    echo $exception->getMessage() . ' ' . $exception->getFile() . ':' . $exception->getLine() . PHP_EOL;
+} finally {
+    echo 'Finally block' . PHP_EOL;
+}
+
+// Global exception handling
+
+set_exception_handler(function (\Throwable $exception) {
+    var_dump($exception->getMessage());
+});
+
+echo array_rand([], 1);
+
+// Or
+
+try {
+    echo array_rand([], 1);
+} catch (\Throwable $exception) {
+    echo $exception->getMessage() . PHP_EOL;
+}
